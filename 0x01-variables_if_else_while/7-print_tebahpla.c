22e8ea583e62c3f2cd5047fc07d17e0ddfe0b22e
Onyema <onyema.bosworth@gmail.com>
2022-10-14 08:07:25 -0700

    shells

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 when everything works
+ *
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 122; n >= 97; --n)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
