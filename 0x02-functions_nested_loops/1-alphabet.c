30bfdd109f10258dcc3430b9a4052a9389ecfc7f
Onyema <onyema.bosworth@gmail.com>
2022-10-18 09:58:23 -0700

    shell

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,9 +9,9 @@
 
 void print_alphabet(void)
 {
-	char alphabe;
+	char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
 	}
