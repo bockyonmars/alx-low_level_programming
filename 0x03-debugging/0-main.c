9834dc4c9349334eb6da5b3e25ad6cba6fff5f32
Onyema <onyema.bosworth@gmail.com>
2022-10-20 01:32:37 -0700

    shell

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks function that prints out if an integer is positive or negative
+ *
+ * Return: returns 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - creates an infinite loop
+ *
+ * Return: returns 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop invoming :(\n");
+
+	i = 0;
+	/* while (i < 10)
+	 * {
+	 *       putchar(i);
+	 * }
+	 */
+
+	printf("INfinite loop avoided! \\o/\n");
+
+	return (0);
+}
