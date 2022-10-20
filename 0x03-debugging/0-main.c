eda51949849a5299f2328e4e9134e87b42f8473e
Onyema <onyema.bosworth@gmail.com>
2022-10-20 07:54:51 -0700

    shell

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * main - checks function that prints out if an integer is positive or negative
- * Return: returns 0 (success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
-
 int main(void)
 {
 	int i;
-	
+
 	i = 0;
 	positive_or_negative(i);
 
