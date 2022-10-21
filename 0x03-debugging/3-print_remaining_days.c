b2908cb0f02cd8956c23094366dc83199428a4a2
Onyema <onyema.bosworth@gmail.com>
2022-10-20 20:00:08 -0700

    last
    g
    fddd
    yy
    git commit -m last

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,11 +13,8 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
-		if (month >= 3 && day >= 60)
-		{
+		if (month > 2)
 			day++;
-		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - days);
 	}
