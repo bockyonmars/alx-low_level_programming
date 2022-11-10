5caf8103bc1f0020939307fa2c9257d2ff61ddec
Onyema <onyema.bosworth@gmail.com>
2022-11-10 14:24:15 -0800

    project

---
+++ 0x0C-more_malloc_free/o-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @n: number of bytes to allocate
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int n)
+{
+	void *ptr;
+
+	ptr = malloc(n);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
