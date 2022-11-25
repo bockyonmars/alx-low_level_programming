182f23773cfb2c7b21afc13d72c12ef33609c6dd
Onyema <onyema.bosworth@gmail.com>
2022-11-24 21:03:11 -0800

    script

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -9,14 +8,15 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
-
+	size_t s;
+	
+	s = 0;
 	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
+		if (h->str== NULL)
+			printf("[%d] %s\n", 0, " (nil)");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
